d1661179dbfc6ff5f85851c0e496e489c5f4b4c7
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-10-03 00:05:31 +0100

    add starter

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />⛔ Page Not Found ⛔
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
