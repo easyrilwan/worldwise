062974785ba385816268aa4953e01389b557bb40
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-10-03 00:38:52 +0100

    page nav committed

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom";
-import style from "./PageNav.module.css";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 export default function PageNav() {
   return (
-    <nav className={style.nav}>
+    <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
